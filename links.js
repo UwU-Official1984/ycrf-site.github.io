0ccc93c85fe2ea78279d4f558bad1e4c250e8b15
UwU-Official1984 <uwuofficial1984@gmail.com>
2025-03-14 23:22:42 -0700

    initial

---
+++ links.js
@@ -0,0 +1,27 @@
+export const links = [
+    {
+        name: "Bitch Beheading Tee (Gorehog)",
+        link:
+        "https://gorehogmerchandise.bigcartel.com/product/uwu-unbridled-slamming-weebgore",
+    },
+    {
+        name: "Spotify",
+        link:
+        "https://open.spotify.com/artist/6U9EOynMGBVG1KQrrrzgkS",
+    },
+    {
+        name: "Bandcamp",
+        link:
+        "https://u-w-u.bandcamp.com",
+    },
+    {
+        name: "Instagram",
+        link:
+        "https://www.instagram.com/u.w.u.official",
+    },
+    {
+        name: "E-mail",
+        link:
+        "mailto:uwuofficial1984@gmail.com",
+    },
+];
